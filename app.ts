2933439b50420a86d098bd9ac037a54ce50075c8
Lucas Lagnes <lucaslagnes@orange.fr>
2024-01-18 08:43:46 +0100

    LL :  correction du swager et ajout du cors

--- app.ts
+++ app.ts
@@ -1,21 +1,25 @@
 import express from 'express';
-//import { validator } from './middlewares/validator';
+import cors from 'cors';
+// import { validator } from './middlewares/validator';
 
 import userRouter from './routers/userRouter';
 import boatRouter from './routers/boatRouter';
 import reservationRouter from './routers/reservationRouter';
 import commentRouter from './routers/commentRouter';
-//import authRouter from './routers/authRouter';
+// import authRouter from './routers/authRouter';
 
 const app = express();
 
-app.use(express.json());
-//app.use(validator);
+// Ajoutez le middleware CORS
+app.use(cors());
 
-//app.use('/auth', authRouter);
+app.use(express.json());
+// app.use(validator);
+
+// app.use('/auth', authRouter);
 app.use('/users', userRouter);
 app.use('/boats', boatRouter);
 app.use('/reservations', reservationRouter);
 app.use('/comments', commentRouter);
 
-export default app ;
+export default app;
